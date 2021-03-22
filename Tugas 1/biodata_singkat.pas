fe4f2d7592c487841df7bb9adb1a79580ff58045
Arieahmadsyarief14 <73570163+Arieahmadsyarief14@users.noreply.github.com>
2021-03-22 15:11:19 +0700

    Create biodata_singkat.pas

---
+++ Tugas 1/biodata_singkat.pas
@@ -0,0 +1,18 @@
+{Nama                 : Arie Ahmad Syarief }
+   {NPM                  : 20.14.1.0031       }
+   {NAMA PROGRAM         : BiodataSingkat     }
+
+
+   program BiodataSingkat;
+   uses crt;
+
+   begin
+   writeln('====================BIODATA======================');
+   writeln('Nama   : Arie Ahmad Syarief');
+   writeln('NPM    : 20.14.10031');
+   writeln('Alamat : blok selasa sinang haji wetan desa panjalin kidul kecamatan sumberjaya');
+   writeln('TTL    : Majalengka,14 maret 2002');
+   writeln('No.HP  : 08986489650');
+   writeln('Motto  : Setiap kesulitan selalu ada kemudahan, Setiap masalah pasti ada solusi');
+   readln;
+   end.
