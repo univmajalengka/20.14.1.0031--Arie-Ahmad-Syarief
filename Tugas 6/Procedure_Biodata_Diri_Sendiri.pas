624866f68db73bca4e29cdb1de3be142a2c7abef
Arieahmadsyarief14 <73570163+Arieahmadsyarief14@users.noreply.github.com>
2021-04-09 16:14:33 +0700

    Create Procedure_Biodata_Diri_Sendiri.pas

---
+++ Tugas 6/Procedure_Biodata_Diri_Sendiri.pas
@@ -0,0 +1,38 @@
+{Nama : Arie Ahmad Syarief}
+{NPM : 20.14.1.0031}
+{Nama Program : Biodata_Diri_Sendiri.pas}
+
+program procedure_biodata;
+uses crt;
+
+
+procedure biodata;
+var
+nama,npm,alamat,ttl,wa,email:string;
+jk:char;
+begin
+writeln('Masukan Nama                :');readln(nama);
+writeln('Masukan NPM                 :');readln(NPM);
+writeln('Masukan jenis kelamin (L/P) :');readln(jk);
+writeln('Masukan Alamat              :');readln(alamat);
+writeln('Masukan Tempat Tanggal Lahir:');readln(ttl);
+writeln('Masukan Kontak Whatsapp     :');readln(wa);
+writeln('Masukan Email               :');readln(email);
+writeln('----------============================----------');
+writeln('----------====BIODATA DIRI SENDIRI====----------');
+Writeln('----------============================----------');
+writeln('Nama                           :',nama);
+writeln('NPM                            :',npm);
+writeln('Jenis Kelamin                  :',jk);
+writeln('Alamat                         :',alamat);
+writeln('Tempat Tanggal Lahir           :',jk);
+writeln('Kontak Whatsapp                :',wa);
+writeln('Email                          :',email);
+end;
+
+begin
+clrscr;
+biodata;
+readln;
+
+end.
